99c9f2884f26bf11c88c83cd2fd020a3e4f3ff1f
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-17 00:56:52 +0200

    bundle header files in wheel

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import Extension, setup
 
 setup(
     include_package_data=True,
+    package_data={'rpeasings': ['*.h']},
     ext_modules=[
-        Extension('rpeasings', ['src_c/rpeasings.c'], include_dirs=['include'])
+        Extension('rpeasings', ['src_c/rpeasings.c'], include_dirs=['src/rpeasings/include'])
     ]
 )
