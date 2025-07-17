5206263dda17488d9c08f9ce598353b5688f80bd
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-17 09:38:51 +0200

    stubs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import Extension, setup
 
 setup(
     include_package_data=True,
-    package_data={'rpeasings': ['*.h']},
+    package_data={'rpeasings': ['*.h', "py.typed"]},
     ext_package = 'rpeasings',
     ext_modules=[
         Extension('_rpeasings', ['src_c/rpeasings.c'], include_dirs=['src/rpeasings/include'])
